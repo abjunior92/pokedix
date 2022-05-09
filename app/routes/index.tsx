1cb4fa798bdd80f4740de268d1b5df366d148e19
abjunior92 <junior.berselli@gmail.com>
2022-05-09 11:02:39 +0200

    try adding meta SEO

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -25,7 +25,21 @@ export const action: ActionFunction = async ({ request }: { request: any }) => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Pokémon App!",
-  viewport: "width=device-width,initial-scale=1"
+  "og:image": "./public/preview.png",
+  "og:title": "Pokémon App!",
+  "og:url": "https://poke-dix.netlify.app/",
+  "og:description": "A simple Pokédex app built with Remix JS",
+  "og:type": "website",
+  "og:image:alt": "Pokémon App!",
+  "twitter:card": "summary_large_image",
+  "twitter:title": "Pokémon App!",
+  "twitter:url": "https://poke-dix.netlify.app/",
+  "twitter:description": "A simple Pokédex app built with Remix JS",
+  "twitter:image": "./public/preview.png",
+  "twitter:image:alt": "Pokémon App!",
+  "twitter:creator": "Andrea Junior Berselli",
+  viewport: "width=device-width,initial-scale=1",
+  description: "A simple Pokédex app built with Remix JS"
 });
 
 // If you want to download images from api, you can use this function:
