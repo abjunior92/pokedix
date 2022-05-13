734bdb4caafa1a8a2bda1b83c520243c34c19988
abjunior92 <junior.berselli@gmail.com>
2022-05-13 16:39:36 +0200

    add evolution chain UI improve

--- app/routes/pokemon/$name.tsx
+++ app/routes/pokemon/$name.tsx
@@ -1,14 +1,21 @@
 import { ArrowNarrowRightIcon } from "@heroicons/react/solid";
 import { LoaderFunction, MetaFunction } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData, useLocation } from "@remix-run/react";
 import {
-  isPokemonEvolutionChain,
   isPokemonValueUnit,
   PokemonDetails,
   PokemonEvolutionChain,
   PokemonValueUnit
 } from "lib/pokemon";
-import { Fragment, ReactNode, useCallback, useMemo } from "react";
+import {
+  Fragment,
+  memo,
+  ReactNode,
+  useCallback,
+  useEffect,
+  useMemo
+} from "react";
+import isEqual from "react-fast-compare";
 import { getPokemonDetails } from "~/endpoints/pokemon-fetch";
 import { formatUnits } from "~/utilities/formatUnits";
 
@@ -30,28 +37,56 @@ export const meta: MetaFunction = ({ data }: { data: any }) => {
   };
 };
 
-const EvolutionChain = ({ chain }: { chain: PokemonEvolutionChain }) => {
-  return (
-    <>
-      {chain.evolves_to.map((item: PokemonEvolutionChain, index: number) => {
-        return (
-          <Fragment key={index}>
-            <span key={item.species.name} className="flex w-full">
-              {item.species.name}
-              {item.evolves_to.length > 0 && (
-                <ArrowNarrowRightIcon className="w-5 h-5 ml-2 text-center" />
-              )}
-            </span>
-            <EvolutionChain key={index} chain={chain.evolves_to[index]} />
-          </Fragment>
-        );
-      })}
-    </>
-  );
-};
+const EvolutionChain = memo(
+  ({ chain, pokeId }: { chain: PokemonEvolutionChain; pokeId: number }) => {
+    const location = useLocation();
+    return (
+      <>
+        {chain.evolves_to.map((item: PokemonEvolutionChain, index: number) => {
+          const idByUrl = item.species.url.split("/").at(-2);
+          return (
+            <Fragment key={index}>
+              <Link
+                to={{
+                  pathname: `/pokemon/${item.species.name}`,
+                  search: location.search
+                }}
+                className="evolution-card"
+              >
+                <div className="evolution-img-card">
+                  <img
+                    className="w-24 h-24"
+                    src={`/pokemonSVG/${idByUrl}.svg`}
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <h5 className="text-xl font-medium text-gray-900 dark:text-s-dark">
+                    {item.species.name}
+                  </h5>
+                </div>
+                <div className="w-full">
+                  <ArrowNarrowRightIcon className="text-indigo-900 dark:text-indigo-400 w-6 h-6" />
+                </div>
+              </Link>
+
+              <EvolutionChain
+                key={index}
+                chain={chain.evolves_to[index]}
+                pokeId={pokeId}
+              />
+            </Fragment>
+          );
+        })}
+      </>
+    );
+  },
+  isEqual
+);
 
 export default () => {
   const data = useLoaderData<PokemonDetails>();
+  const location = useLocation();
 
   const attributes = useMemo(
     () => ({
@@ -68,35 +103,18 @@ export default () => {
       order: data.order,
       "base experience": data.base_experience,
       abilities: data.abilities,
-      types: data.types,
-      "evolution chain": data.evolution_chain
+      types: data.types
     }),
     [data]
   );
 
-  const handleOutputValue = (
-    value:
-      | PokemonValueUnit
-      | string
-      | number
-      | string[]
-      | ReactNode
-      | PokemonEvolutionChain
-  ) => {
-    if (isPokemonEvolutionChain(value)) {
-      return (
-        <div>
-          <span key={value.species.name} className="flex w-full">
-            {value.species.name}
-            {value.evolves_to.length > 0 && (
-              <ArrowNarrowRightIcon className="w-5 h-5 ml-2 text-center" />
-            )}
-          </span>
-          <EvolutionChain chain={value} />
-        </div>
-      );
-    }
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [data]);
 
+  const handleOutputValue = (
+    value: PokemonValueUnit | string | number | string[] | ReactNode
+  ) => {
     if (isPokemonValueUnit(value)) {
       return formatUnits(value?.value, value?.unit);
     } else if (
@@ -118,61 +136,92 @@ export default () => {
   };
 
   const memoizedHandleOutValue = useCallback(
-    (
-      val:
-        | PokemonValueUnit
-        | string
-        | number
-        | string[]
-        | ReactNode
-        | PokemonEvolutionChain
-    ) => handleOutputValue(val),
+    (val: PokemonValueUnit | string | number | string[] | ReactNode) =>
+      handleOutputValue(val),
+    [data]
+  );
+
+  const idByUrl = useMemo(
+    () => data?.evolution_chain.species.url.split("/").at(-2),
     [data]
   );
 
   return (
-    <div className="grid xl:grid-cols-2 gap-8">
-      <div className="div-img-details">
-        <img
-          src={`/pokemonSVG/${data.id}.svg`}
-          alt={`${data.name.toLowerCase()}`}
-        />
-      </div>
-      <div>
-        <div className="grid grid-flow-row">
-          <div className="my-2 overflow-x-auto sm:-mx-6 lg:-mx-6">
-            <div className="py-2 align-middle inline-block w-full sm:px-6 lg:px-8">
-              <div className="shadow overflow-x-auto border-slate-200 rounded-lg">
-                <table className="min-w-full divide-y divide-slate-100 touch-auto">
-                  <thead className="bg-indigo-900">
-                    <tr>
-                      <th scope="col">Attribute</th>
-                      <th scope="col">Value</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {Object.entries(attributes).map(([key, value], idx) => (
-                      <tr
-                        key={key}
-                        className={
-                          idx % 2 === 0
-                            ? "bg-white dark:box-dark"
-                            : "bg-slate-50 dark:bg-h-dark"
-                        }
-                      >
-                        <td className="key-td">{key}</td>
-                        <td className="value-td">
-                          {memoizedHandleOutValue(value)}
-                        </td>
+    <div className="flex flex-col py-10">
+      <div className="grid xl:grid-cols-2 gap-8 mb-4">
+        <div className="div-img-details">
+          <img
+            src={`/pokemonSVG/${data.id}.svg`}
+            alt={`${data.name.toLowerCase()}`}
+          />
+        </div>
+        <div>
+          <div className="grid grid-flow-row">
+            <div className="my-2 overflow-x-auto">
+              <div className="py-2 align-middle inline-block w-full">
+                <div className="shadow overflow-x-auto border-slate-200 rounded-lg">
+                  <table className="min-w-full divide-y divide-slate-100 touch-auto">
+                    <thead className="bg-indigo-900">
+                      <tr>
+                        <th scope="col">Attribute</th>
+                        <th scope="col">Value</th>
                       </tr>
-                    ))}
-                  </tbody>
-                </table>
+                    </thead>
+                    <tbody>
+                      {Object.entries(attributes).map(([key, value], idx) => (
+                        <tr
+                          key={key}
+                          className={
+                            idx % 2 === 0
+                              ? "bg-white dark:box-dark"
+                              : "bg-slate-50 dark:bg-h-dark"
+                          }
+                        >
+                          <td className="key-td">{key}</td>
+                          <td className="value-td">
+                            {memoizedHandleOutValue(value)}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+      {data.evolution_chain && (
+        <div className="w-full">
+          <h3>Evolution chain</h3>
+          <div className="flex flex-col space-y-8 sm:flex-row sm:space-x-8 sm:space-y-0">
+            <Link
+              to={{
+                pathname: `/pokemon/${data.evolution_chain.species.name}`,
+                search: location.search
+              }}
+              className="evolution-card"
+            >
+              <div className="evolution-img-card">
+                <img
+                  className="w-24 h-24"
+                  src={`/pokemonSVG/${idByUrl}.svg`}
+                  alt=""
+                />
+              </div>
+              <div>
+                <h5 className="text-xl font-medium text-gray-900 dark:text-s-dark">
+                  {data.evolution_chain.species.name}
+                </h5>
+              </div>
+              <div className="w-full">
+                <ArrowNarrowRightIcon className="text-indigo-900 dark:text-indigo-400 w-6 h-6" />
+              </div>
+            </Link>
+            <EvolutionChain chain={data.evolution_chain} pokeId={data.id} />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
